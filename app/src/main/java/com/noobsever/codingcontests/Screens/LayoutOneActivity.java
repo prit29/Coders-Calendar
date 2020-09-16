c9bece4bbd5cea01878db7eb2f8fb3a8b8d88d4b
Hemang <hemangnh18@gmail.com>
2020-09-17 03:52:52 +0530

    Screen Folder Refactor
    Signed-off-by: Hemang <hemangnh18@gmail.com>

---
+++ app/src/main/java/com/noobsever/codingcontests/Screens/LayoutOneActivity.java
@@ -0,0 +1,16 @@
+package com.noobsever.codingcontests.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.noobsever.codingcontests.R;
+
+public class LayoutOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_one);
+    }
+}
--- app/src/main/java/com/noobsever/codingcontests/Screens/LayoutTwoActivity.java
+++ app/src/main/java/com/noobsever/codingcontests/Screens/LayoutTwoActivity.java
@@ -1,14 +1,16 @@
-package com.noobsever.codingcontests;
+package com.noobsever.codingcontests.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.noobsever.codingcontests.R;
+
+public class LayoutTwoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_layout_one);
     }
 }
