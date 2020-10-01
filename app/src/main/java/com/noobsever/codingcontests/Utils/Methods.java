5d6418cd6507188c4afe5551ffab16f85b7a1383
Fenil Kamdar <kamdarfenil88@gmail.com>
2020-10-01 16:19:58 +0530

    Added functions to Toast a message

---
+++ app/src/main/java/com/noobsever/codingcontests/Utils/Methods.java
@@ -0,0 +1,18 @@
+package com.noobsever.codingcontests.Utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+class Methods {
+
+    public static void showToast(Context context, String message)          //Function to show short Toast message
+    {
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+
+    public static void showLongToast(Context context, String message)      //Function to show Long Toast message
+    {
+        Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+}
