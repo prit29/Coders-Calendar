44563beebbcd74de1004d79ba137b5a33a299a0e
Ashish <ashishpatel16.2000@gmail.com>
2020-10-01 20:27:13 +0530

    Added Dynamic tab layouts in LayoutOneActivity.

--- app/src/main/java/com/noobsever/codingcontests/Screens/LayoutOneActivity.java
+++ app/src/main/java/com/noobsever/codingcontests/Screens/LayoutOneActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,13 +13,18 @@ import android.view.View;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 import com.noobsever.codingcontests.R;
+
+import java.util.ArrayList;
 
 public class LayoutOneActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     NavigationView mNavigationOne;
     MaterialToolbar mTopbarOne;
     DrawerLayout mDrawerOne;
+    TabLayout mTabLayout;
+    ArrayList<String> mTabItemList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +34,23 @@ public class LayoutOneActivity extends AppCompatActivity implements NavigationVi
         mNavigationOne = findViewById(R.id.navigation_one);
         mTopbarOne = findViewById(R.id.top_bar_one);
         mDrawerOne = findViewById(R.id.drawer_one);
+        mTabLayout = findViewById(R.id.tab_layout);
 
         setSupportActionBar(mTopbarOne);
+
+        // I'm assuming we get a list of strings from Settings for now.
+        if(getIntent().getExtras() != null){
+            mTabItemList = getIntent().getExtras().getStringArrayList("TAB_ITEMS");
+        }else {
+            mTabItemList = new ArrayList<>();
+            mTabItemList.add("Codeforces");
+            mTabItemList.add("Codechef");
+            mTabItemList.add("HackerRank");
+            mTabItemList.add("HackerEarth");
+            mTabItemList.add("SPOJ");
+            mTabItemList.add("AtCoder");
+        }
+        addTabs(); // Populate the tabs.
 
         mNavigationOne.setNavigationItemSelectedListener(this);
 
@@ -50,6 +71,7 @@ public class LayoutOneActivity extends AppCompatActivity implements NavigationVi
         switch (item.getItemId()) {
             case R.id.nav_settings:
                 //mDrawerOne.closeDrawer(GravityCompat.START);
+                startActivity(new Intent(this,Settings.class));
                 break;
             case R.id.nav_notifications:
 
@@ -78,5 +100,12 @@ public class LayoutOneActivity extends AppCompatActivity implements NavigationVi
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    public void addTabs() {
+        // Using a list of strings to dynamically add tabs.
+        for(String s : mTabItemList) {
+            mTabLayout.addTab(mTabLayout.newTab().setText(s));
+        }
     }
 }
--- app/src/main/java/com/noobsever/codingcontests/Screens/Settings.java
+++ app/src/main/java/com/noobsever/codingcontests/Screens/Settings.java
@@ -2,15 +2,50 @@ package com.noobsever.codingcontests.Screens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CheckBox;
 
 import com.noobsever.codingcontests.R;
 
+import java.util.ArrayList;
+
 public class Settings extends AppCompatActivity {
+
+    CheckBox cforces,cchef,hrank,hearth,spoj,atcoder;
+    ArrayList<String> checkedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        checkedItem = new ArrayList<>();
+
+        cforces = findViewById(R.id.cb_codeforces);
+        cchef = findViewById(R.id.cb_codechef);
+        hrank = findViewById(R.id.cb_hackerrank);
+        hearth = findViewById(R.id.cb_hackerearth);
+        spoj = findViewById(R.id.cb_spoj);
+        atcoder = findViewById(R.id.cb_atcoder);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        /** Temporary code to get the project moving. Spaghetti code ALERT! */
+
+        Log.i("debug","going good");
+        if(cforces.isChecked()) checkedItem.add("Codeforces");
+        if(cchef.isChecked()) checkedItem.add("Codechef");
+        if(hrank.isChecked()) checkedItem.add("HackerRank");
+        if(hearth.isChecked()) checkedItem.add("HackerEarth");
+        if(spoj.isChecked()) checkedItem.add("SPOJ");
+        if(atcoder.isChecked()) checkedItem.add("AtCoder");
+
+        Intent i = new Intent(Settings.this,LayoutOneActivity.class);
+        i.putStringArrayListExtra("TAB_ITEMS",checkedItem);
+        startActivity(i);
     }
 }
