d91b8a9f0473070be8e39063c5a624cfd32dde17
Ashish Patel <ashishpatel16.2000@gmail.com>
2020-10-02 03:01:56 +0530

    Added Dynamic Tabs and made use of shared preferences. (#23)
    * Added Dynamic tab layouts in LayoutOneActivity.
    
    * Replaced ArrayList based method with SharedPreferences.
    
    * Replaced ArrayList based method with SharedPreferences.
    
    * Handled Settings activity to display only previously marked checkboxes fetched from sharedpreferences.

--- app/src/main/java/com/noobsever/codingcontests/Screens/LayoutOneActivity.java
+++ app/src/main/java/com/noobsever/codingcontests/Screens/LayoutOneActivity.java
@@ -5,20 +5,33 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
 import com.noobsever.codingcontests.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class LayoutOneActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     NavigationView mNavigationOne;
     MaterialToolbar mTopbarOne;
     DrawerLayout mDrawerOne;
+    TabLayout mTabLayout;
+    ArrayList<String> mTabItemList;
+    SharedPreferences preferences;
+    final String LIST_KEY = "LIST_KEY";
+    final String SHARED_PREFERENCE_KEY = "SHARED_PREFERENCE_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +41,36 @@ public class LayoutOneActivity extends AppCompatActivity implements NavigationVi
         mNavigationOne = findViewById(R.id.navigation_one);
         mTopbarOne = findViewById(R.id.top_bar_one);
         mDrawerOne = findViewById(R.id.drawer_one);
+        mTabLayout = findViewById(R.id.tab_layout);
 
         setSupportActionBar(mTopbarOne);
+
+        /** Storing an ArrayList in SharedPreference using Gson.
+         *  Reference : https://stackoverflow.com/a/27872280/13803511 */
+
+        preferences = getSharedPreferences(SHARED_PREFERENCE_KEY,MODE_PRIVATE);
+
+        try {
+            Gson gson = new Gson();
+            String jsonText = preferences.getString(LIST_KEY, null);
+            String[] text = gson.fromJson(jsonText, String[].class); // could be null
+            mTabItemList = new ArrayList<>();
+            mTabItemList.addAll(Arrays.asList(text));
+
+        }catch (NullPointerException e) {
+            e.printStackTrace();
+
+            // Displays all tabs by Default.
+            mTabItemList = new ArrayList<>();
+            mTabItemList.add("Codeforces");
+            mTabItemList.add("Codechef");
+            mTabItemList.add("HackerRank");
+            mTabItemList.add("HackerEarth");
+            mTabItemList.add("SPOJ");
+            mTabItemList.add("AtCoder");
+        }
+
+        addTabs(); // Populate the tabs.
 
         mNavigationOne.setNavigationItemSelectedListener(this);
 
@@ -50,6 +91,7 @@ public class LayoutOneActivity extends AppCompatActivity implements NavigationVi
         switch (item.getItemId()) {
             case R.id.nav_settings:
                 //mDrawerOne.closeDrawer(GravityCompat.START);
+                startActivity(new Intent(this,Settings.class));
                 break;
             case R.id.nav_notifications:
 
@@ -78,5 +120,12 @@ public class LayoutOneActivity extends AppCompatActivity implements NavigationVi
                 return super.onOptionsItemSelected(item);
         }
         return true;
+    }
+
+    public void addTabs() {
+        // Using a list of strings to dynamically add tabs.
+        for(String s : mTabItemList) {
+            mTabLayout.addTab(mTabLayout.newTab().setText(s));
+        }
     }
 }
