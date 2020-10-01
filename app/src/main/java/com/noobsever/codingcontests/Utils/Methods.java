8d1b50daedc3c07caae37b0e684897816aebaeb3
Fenil Kamdar <kamdarfenil88@gmail.com>
2020-10-01 23:53:14 +0530

    Added function that returns SharedPreferences

--- app/src/main/java/com/noobsever/codingcontests/Utils/Methods.java
+++ app/src/main/java/com/noobsever/codingcontests/Utils/Methods.java
@@ -6,13 +6,18 @@ import android.widget.Toast;
 
 class Methods {
 
-    public static void showToast(Context context, String message)          //Function to show short Toast message
+    public static void showToast(Context context, String message)                   //Function to show short Toast message
     {
         Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
     }
 
-    public static void showLongToast(Context context, String message)      //Function to show Long Toast message
+    public static void showLongToast(Context context, String message)               //Function to show Long Toast message
     {
         Toast.makeText(context,message,Toast.LENGTH_LONG).show();
+    }
+
+    public static SharedPreferences getPreferences(Context context, String key)     //Function to return SharedPreferences
+    {
+        return context.getSharedPreferences(key,Context.MODE_PRIVATE);
     }
 }
