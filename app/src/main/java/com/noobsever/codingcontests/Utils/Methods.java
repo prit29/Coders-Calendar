4329e58ec48eeedf443845445770c3f20b9e82f4
Fenil Kamdar <kamdarfenil88@gmail.com>
2020-10-03 12:39:05 +0530

    Added Functions to retrieve values from SharedPreferences

--- app/src/main/java/com/noobsever/codingcontests/Utils/Methods.java
+++ app/src/main/java/com/noobsever/codingcontests/Utils/Methods.java
@@ -6,39 +6,80 @@ import android.widget.Toast;
 
 class Methods {
 
-    public static void showToast(Context context, String message)                   //Function to show short Toast message
+    /**Function to show short Toast message*/
+    public static void showToast(Context context, String message)
     {
         Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
     }
 
-    public static void showLongToast(Context context, String message)               //Function to show Long Toast message
+    /**Function to show Long Toast message*/
+    public static void showLongToast(Context context, String message)
     {
         Toast.makeText(context,message,Toast.LENGTH_LONG).show();
     }
 
-    public static SharedPreferences getPreferences(Context context, String key)     //Function to return SharedPreferences
+    /**This function returns the String value of a given key stored in SharedPreferences.
+       In case if preference doesn't exist, empty string is returned
+     */
+    public static String getStringPreferences(Context context, String sharedPreferenceKey, String storeKey)
     {
-        return context.getSharedPreferences(key,Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE);
+        return  sharedPreferences.getString(storeKey,"");
     }
-    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, String value)                           //Function to store a String in SharedPreferences
+
+    /**This function returns the integer value of a given key stored in SharedPreferences.
+        In case if preference doesn't exist, 0 is returned
+     */
+    public static int getIntPreferences(Context context, String sharedPreferenceKey, String storeKey)
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE);
+        return  sharedPreferences.getInt(storeKey,0);
+    }
+
+    /**This function returns the floating point value of a given key stored in SharedPreferences.
+       In case if preference doesn't exist, 0.0f is returned
+     */
+    public static float getFloatPreferences(Context context, String sharedPreferenceKey, String storeKey)
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE);
+        return  sharedPreferences.getFloat(storeKey,0.0f);
+    }
+
+    /**This function returns the long value of a given key stored in SharedPreferences.
+        In case if preference doesn't exist, 0 is returned.
+     */
+    public static long getLongPreferences(Context context, String sharedPreferenceKey, String storeKey)
+    {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE);
+        return  sharedPreferences.getLong(storeKey,0);
+    }
+
+    /** Function to store a String value in SharedPreferences*/
+    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, String value)
     {
         SharedPreferences.Editor sharedPreferencesEditor = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE).edit();
         sharedPreferencesEditor.putString(storeKey,value);
         sharedPreferencesEditor.apply();
     }
-    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, int value)                 // Function to store an integer value in SharedPreferences
+
+    /** Function to store an integer value in SharedPreferences*/
+    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, int value)
     {
         SharedPreferences.Editor sharedPreferencesEditor = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE).edit();
         sharedPreferencesEditor.putInt(storeKey,value);
         sharedPreferencesEditor.apply();
     }
-    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, float value)                            //Function to store a floating point value in SharedPreferences
+
+    /** Function to store a float value in SharedPreferences*/
+    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, float value)
     {
         SharedPreferences.Editor sharedPreferencesEditor = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE).edit();
         sharedPreferencesEditor.putFloat(storeKey,value);
         sharedPreferencesEditor.apply();
     }
-    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, long value)                              // Function to store a long value in SharedPreferences
+
+    /** Function to store a long value in SharedPreferences*/
+    public static void setPreferences(Context context, String sharedPreferenceKey, String storeKey, long value)
     {
         SharedPreferences.Editor sharedPreferencesEditor = context.getSharedPreferences(sharedPreferenceKey,Context.MODE_PRIVATE).edit();
         sharedPreferencesEditor.putLong(storeKey,value);
