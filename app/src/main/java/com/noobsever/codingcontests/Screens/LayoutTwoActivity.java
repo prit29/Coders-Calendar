466786372457b0080222874446c96e2e951830b2
Fenil Kamdar <51909055+fenu28@users.noreply.github.com>
2020-10-06 16:52:12 +0530

    Settings Activity exiting bug solved, Added 2 times back press exit with toast message (#37)
    * Added function that returns SharedPreferences
    
    * Added functions to set SharedPreferences
    
    * Added Functions to retrieve values from SharedPreferences
    
    * Added Functions to retrieve values from SharedPreferences
    
    * Activity Stack Bug in Settings Activity solved
    
    * Added 2 times back press exit with toast message

--- app/src/main/java/com/noobsever/codingcontests/Screens/LayoutTwoActivity.java
+++ app/src/main/java/com/noobsever/codingcontests/Screens/LayoutTwoActivity.java
@@ -3,14 +3,33 @@ package com.noobsever.codingcontests.Screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.noobsever.codingcontests.R;
+import com.noobsever.codingcontests.Utils.Methods;
 
 public class LayoutTwoActivity extends AppCompatActivity {
 
+    boolean doubleBackPressExitOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_layout_one);
+    }
+    @Override
+    public void onBackPressed() {
+        if(doubleBackPressExitOnce)
+        {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackPressExitOnce = true;
+        Methods.showToast(this,"Press Back Again to Exit");
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackPressExitOnce = false;
+            }
+        },2000);
     }
 }
