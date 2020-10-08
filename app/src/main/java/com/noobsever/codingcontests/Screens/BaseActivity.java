bf6452b162cea1013de531734124a179474c812b
sanchi0204 <sanchirastogi02@gmail.com>
2020-10-08 16:15:36 +0530

    Base activity for navigation drawer added

---
+++ app/src/main/java/com/noobsever/codingcontests/Screens/BaseActivity.java
@@ -0,0 +1,85 @@
+package com.noobsever.codingcontests.Screens;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.noobsever.codingcontests.R;
+
+public class BaseActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,
+                drawerLayout,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+
+        navigationView.setNavigationItemSelectedListener
+                (new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+
+                        switch (item.getItemId()) {
+
+                            case R.id.nav_settings:
+                                Intent settings = new Intent(getApplicationContext(), Settings.class);
+                                startActivity(settings);
+                                drawerLayout.closeDrawers();
+                                break;
+
+
+
+                        }
+                        return false;
+                    }
+                });
+
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public void finish() {
+        super.finish();
+
+    }
+
+    @Override
+    public void startActivity(Intent intent) {
+        super.startActivity(intent);
+
+    }
+}
