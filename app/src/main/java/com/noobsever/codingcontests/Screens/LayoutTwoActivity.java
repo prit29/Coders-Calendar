c70404ade008f7c60e408bf703f8719a32843139
Dishant Vyas <keenlearner13579@gmail.com>
2020-10-19 23:58:10 +0530

    Added toggle buttons and fragments in second layout.

--- app/src/main/java/com/noobsever/codingcontests/Screens/LayoutTwoActivity.java
+++ app/src/main/java/com/noobsever/codingcontests/Screens/LayoutTwoActivity.java
@@ -5,22 +5,43 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.button.MaterialButtonToggleGroup;
+import com.noobsever.codingcontests.Fragments.fragment1;
+import com.noobsever.codingcontests.Fragments.fragment2;
+import com.noobsever.codingcontests.Fragments.fragment3;
 import com.noobsever.codingcontests.R;
 import com.noobsever.codingcontests.Utils.Constants;
 import com.noobsever.codingcontests.Utils.Methods;
 
 public class LayoutTwoActivity extends BaseActivity {
 
+
     boolean doubleBackPressExitOnce = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         FrameLayout content = findViewById(R.id.content_frame);
         getLayoutInflater().inflate(R.layout.activity_layout_two, content);
-
+        MaterialButtonToggleGroup mbtg = findViewById(R.id.toggle_button_group);
+        mbtg.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+            @Override
+            public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                switch(checkedId) {
+                    case 2131361886: ft.replace(R.id.fragmentSpace, new fragment1()); break;
+                    case 2131361887: ft.replace(R.id.fragmentSpace, new fragment2()); break;
+                    case 2131361888: ft.replace(R.id.fragmentSpace, new fragment3()); break;
+                }
+                ft.commit();
+            }
+        });
         saveActivity();
     }
+
+
     @Override
     public void onBackPressed() {
         if(doubleBackPressExitOnce)
@@ -38,10 +59,12 @@ public class LayoutTwoActivity extends BaseActivity {
         },2000);
     }
 
+
     //  Function to remember current activity.
     public void saveActivity() {
         Methods.setPreferences(this, Constants.LAYOUT_SWITCH_KEY, Constants.CURRENT_ACTIVITY, 2);
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
